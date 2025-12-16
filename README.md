a37bb530d9d0a38d03597040ad2ad8ba31ba3f44
Anand <kashyapanand2020@gmail.com>
2025-12-16 22:17:51 +0530

    Initial project setup with main structure

---
+++ README.md
@@ -0,0 +1,3 @@
+# Student Manager (C++)
+
+A CLI-based student record management system built to practice Git and GitHub workflows.
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Student Manager started" << endl;
+    return 0;
+}
