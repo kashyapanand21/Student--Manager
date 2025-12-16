4be0adae5ddc67aced7565842c4d0cd32998e9be
Anand <kashyapanand2020@gmail.com>
2025-12-17 00:38:43 +0530

    Update menu with search option

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,11 @@ using namespace std;
 
 void showMenu() {
     cout << "1. Add Student" << endl;
-    cout << "2. View Students" << endl;
-    cout << "3. Exit" << endl;
+    cout << "2. Search Student" << endl;
+    cout << "3. View Students" << endl;
+    cout << "4. Exit" << endl;
 }
+
 
 int main() {
     cout << "Student Manager started" << endl;
