fcc85e14849427b244b4a9762cf7ca0a8c3904f6
Anand <kashyapanand2020@gmail.com>
2025-12-16 22:34:58 +0530

    Add basic CLI menu skeleton

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 using namespace std;
 
+void showMenu() {
+    cout << "1. Add Student" << endl;
+    cout << "2. View Students" << endl;
+    cout << "3. Exit" << endl;
+}
+
 int main() {
     cout << "Student Manager started" << endl;
+    showMenu();
     return 0;
 }
